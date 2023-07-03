a56f80543378fdc5b1deca6d09c847d726fb31db
ashishnanaware <117320919+ashishnanaware@users.noreply.github.com>
2023-07-03 15:59:12 +0530

    Update welcome.java

--- welcome.java
+++ welcome.java
@@ -1,7 +1,5 @@
 public class demo{
   public static void main(String []args){
-    System.out.println("this is demo ");
-    System.out.println("this is demo ");
-    System.out.println("this is demo ");
+    System.out.println("This already updated:");
   }
 }
